901e458d52e2ab5852eda82faf65d3461e1c8c7d
borja <borja.a.herrera@gmail.com>
2025-06-12 17:35:17 +0200

    Arreglo el error de rutas 404 en producción

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
-const express = require('express');
 const { connectDB } = require('./src/config/db');
-const mainRouter = require('./src/api/routes/main');
-const cors = require('cors');
 const { configCloudinary } = require('./src/config/cloudinary');
+const mainRouter = require('./src/api/routes/main');
 
 const app = express();
 app.use(cors());
-
 app.use(express.json());
 
 connectDB();
@@ -17,9 +16,20 @@ configCloudinary();
 app.use('/api/v1', mainRouter);
 
 app.use((req, res, next) => {
-  return res.status(404).json('Route not found');
+  const error = new Error('Route not found');
+  error.status = 404;
+  next(error);
+});
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json(err.message || 'Unexpected error');
 });
 
-app.listen(3000, () => {
-  console.log('Servidor levantado en : http://localhost:3000 ✅');
-});
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Servidor en http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
