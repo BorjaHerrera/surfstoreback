9d332df02a3a42ffc02dc711d93df4f8a82f2951
borja <borja.a.herrera@gmail.com>
2025-06-12 23:09:23 +0200

    Actualización para vercel

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 require('dotenv').config();
-
 const express = require('express');
 const { connectDB } = require('./src/config/db');
 const mainRouter = require('./src/api/routes/main');
 const cors = require('cors');
 const { configCloudinary } = require('./src/config/cloudinary');
 
+// Creamos la app
 const app = express();
 app.use(cors());
-
 app.use(express.json());
 
+// Conexiones
 connectDB();
 configCloudinary();
 
+// Rutas
 app.use('/api/v1', mainRouter);
 
+// 404
 app.use((req, res, next) => {
   return res.status(404).json('Route not found');
 });
 
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(3000, () => {
-    console.log('Servidor levantado en : http://localhost:3000 ✅');
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
   });
 }
-
 module.exports = app;
