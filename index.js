1cae353c4f9cce3f9e089ef33fc5e9427dfc6bfa
borja <borja.a.herrera@gmail.com>
2025-06-12 17:18:07 +0200

    Arreglo en el backend para manejo de rutas 404

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const { connectDB } = require('./src/config/db');
+
 const mainRouter = require('./src/api/routes/main');
+
 const cors = require('cors');
+
 const { configCloudinary } = require('./src/config/cloudinary');
 
 const app = express();
+
 app.use(cors());
 
 app.use(express.json());
 
 connectDB();
+
 configCloudinary();
 
 app.use('/api/v1', mainRouter);
 
 app.use((req, res, next) => {
-  return res.status(404).json('Route not found');
+  const error = new Error('Route not found');
+
+  error.status = 404;
+
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json(err.message || 'Unexpected error');
 });
 
 const PORT = process.env.PORT || 3000;
-const BASE_URL = process.env.VERCEL_URL || `http://localhost:${PORT}`;
 
-app.listen(PORT, () => {
-  console.log(`Servidor levantado en: ${BASE_URL}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(
+      `Servidor levantado para desarrollo en: http://localhost:${PORT} ✅`
+    );
+  });
+}
 
 module.exports = app;
