d4318f2aa710c72c5e2ec7404804421f3f2be185
borja <borja.a.herrera@gmail.com>
2025-06-16 22:55:57 +0200

    Eliminar adminProtect de ruta getSongs

--- src/api/routes/songs.js
+++ src/api/routes/songs.js
@@ -12,7 +12,7 @@ const songsRouter = require('express').Router();
 
 const adminProtect = [isAuth, isAdmin];
 
-songsRouter.get('/', adminProtect, getSongs);
+songsRouter.get('/', getSongs);
 songsRouter.get('/:id', adminProtect, getSongById);
 songsRouter.post('/', adminProtect, musicUpload.single('audio'), postSong);
 songsRouter.put('/:id', adminProtect, musicUpload.single('audio'), putSong);
